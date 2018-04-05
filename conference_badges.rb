d25cd33b83b443625313e055b949123e676ebdd4
Marcus Jun <mjun@calstate.edu>
2018-04-05 02:49:05 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,5 +5,13 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  array.collect{}|name| badge_maker(name)}
+  array.collect{|name| badge_maker(name)}
+end
+
+def assign_rooms(array)
+  room=1
+  array.each do |name|
+    puts "Hello, ${name}! You'll be assigned to room ${room}!"
+    room+=1
+  end
 end

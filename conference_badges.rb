fef1ee1d23b0b419f1849fa4585b121d21fb4e52
Marcus Jun <mjun@calstate.edu>
2018-04-05 03:27:39 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,6 +21,6 @@ def assign_rooms(attendees)
 end
 
 def printer (attendees)
-  puts batch_badge_creator(attendees).inspect
-  puts assign_rooms(attendees).inspect
+  puts batch_badge_creator(attendees)#.inspect
+  puts assign_rooms(attendees)#.inspect
 end

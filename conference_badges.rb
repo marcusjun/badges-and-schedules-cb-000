e1099843cb0b0e6b2a1fb4fb63525553abb4449a
Marcus Jun <mjun@calstate.edu>
2018-04-05 03:12:08 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ end
 def assign_rooms(attendees)
   #room=1
   attendees.each_with_index do |name,index|
-    attendees[index]="Hello, #{name}! You'll be assigned to room #{}{index+1}!"
+    attendees[index]="Hello, #{name}! You'll be assigned to room #{index+1}!"
 
   end
     #{}"Hello, #{name}! You'll be assigned to room #{room}!"
